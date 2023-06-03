055d0eacbb7968b12c2cc67cc55f9a4d02ffd697
楓 <r0yalist@outlook.com>
2023-06-03 11:43:11 +0800

    Updated to Minecraft 1.19.4

--- src/main/java/cn/taskeren/minequery/command/client_pos/ClientPosArgument.java
+++ src/main/java/cn/taskeren/minequery/command/client_pos/ClientPosArgument.java
@@ -3,13 +3,15 @@ package cn.taskeren.minequery.command.client_pos;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 
 public interface ClientPosArgument {
 
 	Vec3d toAbsolutePos(FabricClientCommandSource source);
 
 	default BlockPos toAbsoluteBlockPos(FabricClientCommandSource source) {
-		return new BlockPos(this.toAbsolutePos(source));
+		var p = this.toAbsolutePos(source);
+		return new BlockPos(new Vec3i((int) p.x, (int) p.y, (int) p.z));
 	}
 
 }
