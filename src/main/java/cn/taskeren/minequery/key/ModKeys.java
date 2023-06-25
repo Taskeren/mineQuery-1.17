76ec7df637c37f23df989595b26011b7d64f9e1e
Taskeren <r0yalist@outlook.com>
2023-06-25 18:37:22 +0800

    Backport of ModConfigMenu key

--- src/main/java/cn/taskeren/minequery/key/ModKeys.java
+++ src/main/java/cn/taskeren/minequery/key/ModKeys.java
@@ -1,8 +1,18 @@
 package cn.taskeren.minequery.key;
 
+import cn.taskeren.minequery.MineQueryMod;
+import cn.taskeren.minequery.config.MineQueryConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import org.apache.commons.compress.utils.Lists;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_M;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_Y;
 
 public class ModKeys {
@@ -10,14 +20,44 @@ public class ModKeys {
 	public static final String KEY_CATEGORY = "keybinding.minequery";
 
 	public static KeyBinding keyFeedEm;
+	public static KeyBinding openModConfig;
 
 	public static void init() {
+		// register ticking listener
+		ClientTickEvents.END_CLIENT_TICK.register(ModKeys::tick);
+
 		keyFeedEm = registerKey("feed_em", GLFW_KEY_Y);
+		openModConfig = registerKey("open_config", GLFW_KEY_M, cli -> {
+			var screen = AutoConfig.getConfigScreen(MineQueryConfig.class, cli.currentScreen).get();
+			cli.setScreen(screen);
+		});
 	}
 
 	static KeyBinding registerKey(String keyId, int keyCode) {
 		return KeyBindingHelper
 				.registerKeyBinding(new KeyBinding("keybinding.minequery."+keyId, keyCode, KEY_CATEGORY));
+	}
+
+	private static final List<Consumer<MinecraftClient>> tickList = Lists.newArrayList();
+
+	public static KeyBinding registerKey(String keyId, int keyCode, Consumer<MinecraftClient> onPressed) {
+		var kb = registerKey(keyId, keyCode);
+		tickList.add(client -> {
+			if(kb.isPressed()) {
+				onPressed.accept(client);
+			}
+		});
+		return kb;
+	}
+
+	private static void tick(MinecraftClient client) {
+		tickList.forEach(toTick -> {
+			try {
+				toTick.accept(client);
+			} catch (Exception ex) {
+				MineQueryMod.LOGGER.warn("Exception occurred when ticking keys.", ex);
+			}
+		});
 	}
 
 }
